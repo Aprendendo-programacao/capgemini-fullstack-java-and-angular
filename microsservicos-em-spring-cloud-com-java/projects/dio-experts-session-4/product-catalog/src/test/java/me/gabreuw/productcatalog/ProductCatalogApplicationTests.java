bbadfade08ba85106e448aa14e1ea9f2b39c9c94
ImGabreuw <imgabreuw.dev@gmail.com>
2021-08-31 16:55:33 -0300

    Aula: 'Conhecendo o Ecossistema Spring Boot e Criando um Microsserviço de Catálogo de Produtos'

---
+++ microsservicos-em-spring-cloud-com-java/projects/dio-experts-session-4/product-catalog/src/test/java/me/gabreuw/productcatalog/ProductCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package me.gabreuw.productcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
